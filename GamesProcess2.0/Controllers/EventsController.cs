bcded54ba770918c266c2db5ffe9c600fd75a57b
JESUSEYITAN ODUKOYA <setoryz@gmail.com>
2020-03-22 05:46:50 +0100

    - Events controller added (controllers) - Paginated list added (libs)

---
+++ GamesProcess2.0/Controllers/EventsController.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GamesProcess2.Data;
+using GamesProcess2.Libs;
+using GamesProcess2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GamesProcess2.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly GameContext _context;
+        public int pageSize = 50;
+
+        public EventsController(GameContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Events
+        public async Task<IActionResult> Index(string sortOrder, string currentFilter, string searchString, int? pageNumber)
+        {
+
+            ViewData["CurrentSort"] = sortOrder;
+            ViewData["DateSortParm"] = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+            {
+                pageNumber = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewData["CurrentFilter"] = searchString;
+
+            var events = from s in _context.Events select s;
+
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                events = events.Where(s => s.Winning.Contains(int.Parse(searchString)) || s.Machine.Contains(int.Parse(searchString)));
+            }
+
+            switch (sortOrder)
+            {
+                case "date_desc":
+                    events = events.OrderByDescending(s => s.Date);
+                    break;
+                default:
+                    events = events.OrderBy(s => s.Date);
+                    break;
+            }
+
+            return View(await PaginatedList<Event>.CreateAsync(events.AsNoTracking(), pageNumber ?? 1, pageSize));
+        }
+
+        #region GET: Events/Details
+        // GET: Events/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Events
+                .SingleOrDefaultAsync(m => m.EventID == id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return View(@event);
+        }
+        #endregion
+
+        #region GET: Events/Create
+        // GET: Events/Create
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Create()
+        {
+            return View();
+        }
+        #endregion
+
+        #region POST: Events/Create
+        // POST: Events/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EventID,GameID,Date,WinningValues,MachineValues")] Event @event)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(@event);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(@event);
+        }
+        #endregion
+
+        #region GET: Events/Edit
+        // GET: Events/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Events.SingleOrDefaultAsync(m => m.EventID == id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+            return View(@event);
+        }
+        #endregion
+
+        #region POST: Events/Edit
+        // POST: Events/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("EventID,GameID,Date,WinningValues,MachineValues")] Event @event)
+        {
+            if (id != @event.EventID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(@event);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EventExists(@event.EventID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            return View(@event);
+        }
+        #endregion
+
+        #region GET: Events/Delete
+        // GET: Events/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var @event = await _context.Events
+                .SingleOrDefaultAsync(m => m.EventID == id);
+            if (@event == null)
+            {
+                return NotFound();
+            }
+
+            return View(@event);
+        }
+        #endregion
+
+        #region POST: Events/Delete
+        // POST: Events/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var @event = await _context.Events.SingleOrDefaultAsync(m => m.EventID == id);
+            _context.Events.Remove(@event);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        private bool EventExists(int id)
+        {
+            return _context.Events.Any(e => e.EventID == id);
+        }
+        #endregion
+    }
+}
---
+++ GamesProcess2.0/Libs/PaginatedList.cs
@@ -0,0 +1,81 @@
+﻿using GamesProcess2.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamesProcess2.Libs
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public int PageIndex { get; private set; }
+        public int TotalPages { get; private set; }
+        public List<Game> GamesList { get; private set; }
+        public List<GamesClass> GamesGroups { get; private set; }
+        public int GameSelection { get; private set; }
+        public int GroupSelection { get; private set; }
+
+
+
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            this.AddRange(items);
+        }
+
+        public PaginatedList(List<T> items, int count, int pageIndex, int pageSize, List<Game> _gamesList, List<GamesClass> _gamesGroups, int _gameSelection, int _groupSelection)
+        {
+
+            GamesList = _gamesList;
+            GamesGroups = _gamesGroups;
+            GameSelection = _gameSelection;
+            GroupSelection = _groupSelection;
+
+            PageIndex = pageIndex;
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            this.AddRange(items);
+        }
+
+        //public int gamesID { get; set; }
+
+        // to know if previous page is available
+        public bool HasPreviousPage
+        {
+            get
+            {
+                return (PageIndex > 1);
+            }
+        }
+
+
+
+
+        // to know if next page is available
+        public bool HasNextPage
+        {
+            get
+            {
+                return (PageIndex < TotalPages);
+            }
+        }
+
+        // takes page size and number and applies skip and take statement to the list
+        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize/*, List<Game> _gamesList*/)
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize/*, _gamesList*/);
+        }
+
+        public static PaginatedList<T> Create(IQueryable<T> source, int pageIndex, int pageSize, List<Game> _gamesList, List<GamesClass> _gamesGroups, int _gameSelection, int _groupSelection)
+        {
+            var count = source.Count();
+            var items = source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+            return new PaginatedList<T>(items, count, pageIndex, pageSize, _gamesList, _gamesGroups, _gameSelection, _groupSelection);
+        }
+    }
+}
